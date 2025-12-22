0495f20baa090989003708aef5fcf4949c5445cf
Bimbok <bimbokmkj@gmail.com>
2025-12-22 19:45:19 +0530

    server-fix

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: process.env.CLIENT_URL || 'http://localhost:5173', // Vite default port
     credentials: true
 }));
 app.use(express.json());
